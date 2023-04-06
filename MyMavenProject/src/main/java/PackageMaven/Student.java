65205529e02c043fd136bacb4b4b3ea05388bbef
anura <anura@RIDHU>
2023-04-06 17:54:34 +0530

    Commit one

---
+++ MyMavenProject/src/main/java/PackageMaven/Student.java
@@ -0,0 +1,12 @@
+package PackageMaven;
+
+public class Student {
+static String college="CET";
+	public static void main(String[] args) {
+Student s1=new Student();
+Student s2=new Student();
+System.out.println(college);
+
+	}
+
+}
