33b89afc767273d5214538042d1d6b556b298ede
anura <anura@RIDHU>
2023-04-06 18:01:27 +0530

    update commit one

--- MyMavenProject/src/main/java/PackageMaven/Student.java
+++ MyMavenProject/src/main/java/PackageMaven/Student.java
@@ -5,6 +5,7 @@ static String college="CET";
 	public static void main(String[] args) {
 Student s1=new Student();
 Student s2=new Student();
+Student s3=new Student();
 System.out.println(college);
 
 	}
